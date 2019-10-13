bfdf2998d1bbf3923a424d067d4b7afddd78c964
GermanVor <vorotnikov.german@yandex.ru>
2019-10-13 05:24:47 +0300

    Решил проблему коллекций - дело осталось за малым!

--- app.js
+++ app.js
@@ -1,23 +1,79 @@
 const mongoose = require("mongoose");
+
 const express = require("express");
 const Schema = mongoose.Schema;
 const app = express();
 const jsonParser = express.json();
  
-const userScheme = new Schema({name: String, age: Number}, {versionKey: false});
+const userScheme = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    club: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Club'
+    },
+},{versionKey: false});
+
+const clubScheme = new Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    title: String,
+    numbers: [
+        {   
+            serialNumber: { "type": Number, "default": 1 },
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'Member'
+        }
+    ],
+},{versionKey: false});
+
 const User = mongoose.model("User", userScheme);
- 
+const Club = mongoose.model('Clubs', clubScheme);
+
 app.use(express.static(__dirname + "/public"));
- 
-mongoose.connect("mongodb://localhost:27017/usersdb", { useNewUrlParser: true }, function(err){
-    if(err) return console.log(err);
-    app.listen(3001, function(){
-        console.log("Сервер ожидает подключения...");
-    });
+
+let PugAbbey = new Club({
+    _id: new mongoose.Types.ObjectId(),
+    title: 'Pug Abbey',
+    numbers: []
 });
-  
+
+let CorgiAbbey = new Club({
+    _id: new mongoose.Types.ObjectId(),
+    title: 'Corgi Abbey',
+    numbers: []
+});
+
+mongoose.connect("mongodb://localhost:27017/dogExhibitiondb", { useNewUrlParser: true }, function(err){
+    if(err) return console.log(err);
+
+    Club.findOne({title : 'Pug Abbey' }, function(err, club){
+        if(err) return console.log(err); 
+        if(! club) PugAbbey.save();
+        else {
+            PugAbbey =  club;
+        }
+    });
+    Club.findOne({title : 'Corgi Abbey' }, function(err, club){
+        if(err) return console.log(err); 
+      
+        if(! club) CorgiAbbey.save();
+        else CorgiAbbey =  club;
+    });
+
+    app.listen(3001, function(){
+        console.log("Сервер ожидает подключения...http://localhost:3001");
+    });
+    
+});
+
 app.get("/api/users", function(req, res){
-        
+
     User.find({}, function(err, users){
  
         if(err) return console.log(err);
@@ -41,10 +97,21 @@ app.post("/api/users", jsonParser, function(req, res) {
         
     const userName = req.body.name;
     const userAge = req.body.age;
-    const user = new User({name: userName, age: userAge});
-        
+    //const club = req.body.club;
+    const user = new User({name: userName, age: userAge, club: PugAbbey._id}); // club 
+    
     user.save(function(err){
         if(err) return console.log(err);
+
+        Club.findOneAndUpdate(
+            { title: 'PugAbbey' }, //club
+            function(err, club) {
+                if (err) throw err;
+                club.numbers.push(use._id);
+                // club.save(done);
+        });
+        
+
         res.send(user);
     });
 });
@@ -66,7 +133,7 @@ app.put("/api/users", jsonParser, function(req, res){
     const userName = req.body.name;
     const userAge = req.body.age;
     const newUser = {age: userAge, name: userName};
-     
+    
     User.findOneAndUpdate({_id: id}, newUser, {new: true}, function(err, user){
         if(err) return console.log(err); 
         res.send(user);
